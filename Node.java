81570c49951f64df77e48d5fb0c72e0d5862ea2d
Jonathan <>
2021-01-06 13:09:44 -0500

    coded get

--- Node.java
+++ Node.java
@@ -2,6 +2,9 @@ public class Node{
   private String data;
   private Node next, prev;
   public Node(String value){
-    value = data;
+    data = value;
+  }
+  public String get(){
+    return data;
   }
 }
